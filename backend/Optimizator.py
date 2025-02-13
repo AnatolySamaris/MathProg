106d98322ac2d901cc8d7756ca716da33699c451
Олеся Седых <raskard@yandex.ru>
2025-02-13 20:29:50 +0300

    убрала лишние принты и комментарии

--- backend/Optimizator.py
+++ backend/Optimizator.py
@@ -19,51 +19,24 @@ class Optimizator:
         x_low = np.array(x_low)
         x_high = np.array(x_high)
 
-        # print("="*20, '\n', x_low, x_high, n_vars, '\n', '='*20)
         x_min = np.random.uniform(low=x_low, high=x_high, size=n_vars)
         glob_history = [x_min.copy()]
         symmetry = True
         centers = (x_low + x_high) / 2
 
         for _ in range(N):
-            # if _ % 1000 == 0: print(_)
             x = np.random.uniform(low=x_low, high=x_high, size=n_vars)
             if x_min[0] is None or f(x) < f(x_min):
                 x_min = x
                 glob_history.append(x_min)
             if f(x) - f(2 * centers - x) > 1e-6: symmetry = False
 
-        print(glob_history[-1])
-        print(f(glob_history[-1]))
         x_min, loc_history = loc_method(f, x_min, x_low, x_high, *args, **kwargs)
         if symmetry:
             if np.all(x_min - centers) < 1e-4: symmetry = None
             else: symmetry = 2 * centers - x_min
         else: symmetry = None
         return (x_min, glob_history, loc_history, symmetry)
-
-    # def monte_karlo(f, n_vars: int, x_low: list, x_high: list, N: int, loc_method, *args, **kwargs):
-    #     """
-    #     Ищет глобальный минимум функции методом Монте-Карло.
-    #     В качестве параметров принимает:
-    #         f - оптимизируемая функция
-    #         n_vars - число переменных
-    #         x_low, x_high - списки нижней и верхней границ x
-    #         N - число итераций
-    #         loc_method - метод локальной оптимизацииS
-    #         *args, **kwargs - аргументы метода локальной оптимизации
-    #     """
-    #     x_min = np.random.uniform(low=x_low, high=x_high, size=n_vars)
-    #     glob_history = [x_min.copy()]
-
-    #     for _ in range(N):
-    #         x = np.random.uniform(low=x_low, high=x_high, size=n_vars)
-    #         if x_min[0] is None or f(x) < f(x_min):
-    #             glob_history.append(x_min)
-    #             x_min = x
-
-    #     x_min, loc_history = loc_method(f, x_min, x_low, x_high, *args, **kwargs)
-    #     return (x_min, glob_history, loc_history)
     
     def annealing_imitation(f, n_vars: int, x_low: list, x_high: list, T_max: float, L: int, r: float, eps: float, loc_method, *args, **kwargs):
         """
@@ -79,7 +52,6 @@ class Optimizator:
             loc_method - метод локальной оптимизации
             *args, **kwargs - аргументы метода локальной оптимизации
         """
-        print("annealing", T_max, L, r, eps)
         T = T_max
         x_low = np.array(x_low)
         x_high = np.array(x_high)
@@ -100,8 +72,6 @@ class Optimizator:
                 if f(x) - f(2 * centers - x) > 1e-6: symmetry = False
             T = r * T
         
-        print(glob_history[-1])
-        print(f(glob_history[-1]))
         x_min, loc_history = loc_method(f, x_min, x_low, x_high, *args, **kwargs)
         if symmetry:
             if np.all(abs(x_min - centers)) < 1e-4: symmetry = None
@@ -120,8 +90,6 @@ class Optimizator:
             eps_loc - значение для критерия останова
             N_loc - максимальное число итераций
         """
-        print(x_start, x_low, x_high, eps_loc, N_loc)
-        print(f(x_start))
         loc_history = [x_start.copy()]
         def callback(x):
             x = np.clip(x, x_low, x_high)
@@ -151,8 +119,6 @@ class Optimizator:
             eps_loc - значение для критерия останова
             N_loc - максимальное число итераций
         """
-        print(x_start, x_low, x_high, eps_loc, N_loc)
-        print(f(x_start))
         loc_history = [x_start.copy()]
         def callback(x):
             x = np.clip(x, x_low, x_high)
@@ -184,8 +150,6 @@ class Optimizator:
             N_loc - максимальное число итераций
             h - шаг для численного вычисления якобиана
         """
-        print(x_start, x_low, x_high, eps_loc, N_loc, h)
-        print(f(x_start))
         loc_history = [x_start.copy()]
         def callback(x):
             loc_history.append(x.copy())
@@ -216,8 +180,6 @@ class Optimizator:
             eps_loc - критерий остановки (изменение нормы градиента)
             h - шаг для численного вычисления градиента
         """
-        print(x_start, x_low, x_high, learning_rate, eps_loc, N_loc, h)
-        print(f(x_start))
         x_min = x_start.copy()
         loc_history = [x_min.copy()]
 
@@ -256,8 +218,6 @@ class Optimizator:
             eps_loc - значение для критерия останова
             h - шаг для численного вычисления градиента
         """
-        print(x_start, x_low, x_high, eps_loc, h)
-        print(f(x_start))
         loc_history = [x_start.copy()]
         def callback(x):
             loc_history.append(x.copy())
