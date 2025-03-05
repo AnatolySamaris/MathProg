9f700634ee5c4eb1112b5cbc950b17e960ff44d1
Anatoly Samaris <anatolysamaris@gmail.com>
2025-03-05 16:00:18 +0300

    Сделал пару обслуживающих функций

--- backend/IntervalOptimizer.py
+++ backend/IntervalOptimizer.py
@@ -1,20 +1,35 @@
 import numpy as np
 
 class IntervalOptimizer:
-    def __init__(self):
-        pass
+    def __init__(self, eps: float):
+        self.eps = eps
 
-    def simple_optimize(self, func, n_vars: int, x_low: list, x_high: list, eps: float) -> tuple:
+    def simple_optimize(self, func, n_vars: int, x_low: list, x_high: list) -> tuple:
         pass
 
     def full_optimize(self, func, n_vars: int, x_low: list, x_high: list, eps: float) -> tuple:
         pass
 
+    def __mid(self, box: list) -> list:
+        """
+        Возвращает вектор-середину бруса.
+        Брус - список кортежей, где каждый кортеж - интервал по i-й переменной
+        (Например: [(-1, 1), (-2, 2), (-3, 3)] - брус из трех переменных)
+        """
+        box = np.array(box)
+        return np.mean(box, axis=1)
+
     def __estimate_function(self, func, x_low, x_high) -> tuple:
         """
         Оценка функции включения, возвращает интервал значений функции.
         """
-        pass
+        num_points = int(1 / self.eps)
+        points = np.column_stack([np.linspace(low, high, num_points) 
+                                for low, high in zip(x_low, x_high)])
+        func_values = func(points)
+        min_value = func_values.min()
+        max_value = func_values.max()
+        return (min_value, max_value)
 
     def __centered_estimation(self, func, x_low, x_high) -> tuple:
         """
@@ -22,7 +37,7 @@ class IntervalOptimizer:
         """
         pass
 
-    def __middle_point_test(self) -> bool:
+    def __middle_point_test(self, func, box) -> bool:
         """
         Тест на значение в средней точке.
         """
